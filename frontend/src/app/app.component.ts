be761fbc842504cc1fa41443a0e4a26dd64fcfca
Flávio <flaviojunior0899@gmail.com>
2020-06-19 11:48:21 -0300

    front: building app interface logic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MainService } from './services/main.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+
+  constructor(
+    private mainService: MainService
+  ) { }
+  
+  desiredResponseStatus = 200;
+
+  onSubmit(): void {
+    if (this.desiredResponseStatus < 100 || this.desiredResponseStatus >= 600) {
+      alert('Please, insert a value between 100 and 599');
+      return;
+    }
+    this.mainService
+      .makeRequest(this.desiredResponseStatus)
+      .subscribe(res => alert('Hello! I\'m the main app!'));
+  }
+
 }
